06835894ed7a051b7c742d2fabf170bde8e483b7
Dan Yee <dyee3435@gmail.com>
2022-10-17 14:21:24 -0400

    added console app for testing Client side actions

---
+++ ClientTest/ClientTest.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Net.Client;
+using Server;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7123");                                 // localhost for testing purposes
+var client = new Login.LoginClient(channel);
+
+bool isSuccessfulLogin = false;
+do
+{
+    Console.Write("Username: ");
+    string? username = Console.ReadLine();
+    Console.Write("Password: ");
+    string? password = Console.ReadLine();
+
+    var credentials = new Credentials
+    {
+        Username = username,
+        Password = password,
+    };
+
+    var serviceResponse = await client.doLoginAsync(credentials);                               // assynchronous rpc to Server to verify login credentials
+    if(serviceResponse.IsSuccessfulLogin)                                                       // successful login
+    {
+        isSuccessfulLogin = true;
+        Console.WriteLine("Login Successful!\n");
+    }
+    else                                                                                        // incorrect login credentials
+    {
+        Console.WriteLine("Incorrect username or password. Please try again.\n");
+    }
+} while (!isSuccessfulLogin);
+Console.WriteLine("Press any key to exit...");
+Console.ReadLine();
