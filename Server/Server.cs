98696c56cec6977a17e584ca42ebf48e802b01c9
Dan-Yee <dyee3435@gmail.com>
2022-10-21 13:24:27 -0400

    updated Server implementation to map the new gRPC service, EmployeeService

--- Server/Server.cs
+++ Server/Server.cs
@@ -6,7 +6,7 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<LoginService>();
+app.MapGrpcService<EmployeeService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
