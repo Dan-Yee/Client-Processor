eaf3439bb189885a5b91ab5c98bc9551982e07f5
Dan-Yee <dyee3435@gmail.com>
2022-10-22 22:49:21 -0400

    added comments to Clients.sql file

--- Database/Clients.sql
+++ Database/Clients.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Clients (
 	client_ID serial PRIMARY KEY,
-	first_name varchar(50) NOT NULL,
-	last_name varchar(50) NOT NULL,
-	phone_number varchar(15),
-	email_address varchar(50)
+	first_name varchar(50) NOT NULL,			-- Client's first name
+	last_name varchar(50) NOT NULL,				-- Client's last name
+	phone_number varchar(15),					-- Client's phone number
+	email_address varchar(50)					-- (Optional) Client's email address
 );
