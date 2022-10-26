ba7c16f6a8391445b1f54c731a90dffa01f2e255
Dan Yee <dyee3435@gmail.com>
2022-10-26 14:49:53 -0400

    updated ClientTest file to reflect Server-side changes

--- ClientTest/ClientTest.cs
+++ ClientTest/ClientTest.cs
@@ -2,7 +2,7 @@
 using Server;
 
 var channel = GrpcChannel.ForAddress("https://localhost:7123");                                 // localhost for testing purposes
-var client = new Client.ClientClient(channel);
+/*var client = new Client.ClientClient(channel);
 
 bool continueProgram = false;
 do
@@ -82,40 +82,14 @@ static void listAllClients(AllClients info)
     }
     Console.WriteLine("----------< End All Clients >----------");
     Console.WriteLine();
-}
+}*/
 
 // <BELOW> TESTS FOR EMPLOYEE SERVICES <BELOW>
-/*var client = new Employee.EmployeeClient(channel);
-
-bool isSuccessfulLogin = false;
-do
-{
-    Console.Write("Username: ");
-    string? username = Console.ReadLine();
-    Console.Write("Password: ");
-    string? password = Console.ReadLine();
-
-    var credentials = new LoginCredentials
-    {
-        Username = username,
-        Password = password,
-    };
-
-    var serviceResponse = await client.doLoginAsync(credentials);                               // assynchronous rpc to Server to verify login credentials
-    if(serviceResponse.IsSuccessfulLogin)                                                       // successful login
-    {
-        isSuccessfulLogin = true;
-        Console.WriteLine("Login Successful!\n");
-    }
-    else                                                                                        // incorrect login credentials
-    {
-        Console.WriteLine("Incorrect username or password. Please try again.\n");
-    }
-} while (!isSuccessfulLogin);
-
+var client = new Employee.EmployeeClient(channel);
 bool continueProgram = false;
 do
 {
+    login(client);
     displayActions();
     Console.Write("Your Choice: ");
     string? input = Console.ReadLine();
@@ -127,21 +101,23 @@ do
             var createResponse = client.newEmployee(getNewInfo());
             if(createResponse.IsSuccessfulOperation)
             {
-                Console.WriteLine("Successfully created a new Employee.\n");
+                Console.WriteLine("Successfully created a new Employee.");
             } else
             {
-                Console.WriteLine("Error creating a new Employee.\n");
+                Console.WriteLine("Error creating a new Employee.");
             }
+            Console.WriteLine(createResponse.StatusMessage + "\n");
             break;
         case 2:             // updating an existing employee
             var updateResponse = client.updateEmployee(getUpdatedInfo());
             if(updateResponse.IsSuccessfulOperation)
             {
-                Console.WriteLine("Successfully updated Employee information.\n");
+                Console.WriteLine("Successfully updated Employee information.");
             } else
             {
-                Console.WriteLine("Error updating Employee information.\n");
+                Console.WriteLine("Error updating Employee information.");
             }
+            Console.WriteLine(updateResponse.StatusMessage + "\n");
             break;
         case 3:             // listing all employees
             listAllEmployees(client.getEmployees(new Google.Protobuf.WellKnownTypes.Empty()));
@@ -157,6 +133,37 @@ do
 
 Console.WriteLine("Press any key to exit...");
 Console.ReadLine();
+
+static void login(Employee.EmployeeClient client)
+{
+    bool isSuccessfulLogin = false;
+    do
+    {
+        Console.Write("Username: ");
+        string? username = Console.ReadLine();
+        Console.Write("Password: ");
+        string? password = Console.ReadLine();
+
+        var credentials = new LoginCredentials
+        {
+            Username = username,
+            Password = password,
+        };
+
+        var serviceResponse = client.doLogin(credentials);                               // asynchronous rpc to Server to verify login credentials
+        if (serviceResponse.IsSuccessfulLogin)                                            // successful login
+        {
+            isSuccessfulLogin = true;
+            Console.WriteLine("Login Successful!");
+            Console.WriteLine("You have logged in with Employee ID: " + serviceResponse.EmployeeId);
+            Console.WriteLine("Account Privilege (isAdmin): " + serviceResponse.IsAdmin + "\n");
+        }
+        else                                                                             // incorrect login credentials
+        {
+            Console.WriteLine("Incorrect username or password. Please try again.\n");
+        }
+    } while (!isSuccessfulLogin);
+}
 
 static void displayActions()
 {
@@ -178,6 +185,13 @@ static EmployeeInfo getNewInfo()
     string? newUsername = Console.ReadLine();
     Console.Write("Password: ");
     string? newPassword = Console.ReadLine();
+    Console.Write("Is Admin? (y/n): ");
+    string? admin = Console.ReadLine();
+    bool isAdmin = false;
+    if (admin.Equals("y"))
+    {
+        isAdmin = true;
+    }
 
     var newCredentials = new LoginCredentials
     {
@@ -188,7 +202,8 @@ static EmployeeInfo getNewInfo()
     {
         FirstName = newFirstName,
         LastName = newLastName,
-        Credentials = newCredentials
+        Credentials = newCredentials,
+        IsAdmin = isAdmin
     };
     return employeeInfo;
 }
@@ -207,6 +222,13 @@ static EmployeeInfo getUpdatedInfo()
     string? newUsername = Console.ReadLine();
     Console.Write("Password: ");
     string? newPassword = Console.ReadLine();
+    Console.Write("Is Admin? (y/n): ");
+    string? admin = Console.ReadLine();
+    bool isAdmin = false;
+    if (admin.Equals("y"))
+    {
+        isAdmin = true;
+    }
 
     var newCredentials = new LoginCredentials
     {
@@ -218,12 +240,13 @@ static EmployeeInfo getUpdatedInfo()
         EmployeeId = id,
         FirstName = newFirstName,
         LastName = newLastName,
-        Credentials = newCredentials
+        Credentials = newCredentials,
+        IsAdmin = isAdmin
     };
     return employeeInfo;
 }
 
-static void listAllEmployees(AllEmployeesInfo info)
+static void listAllEmployees(AllEmployees info)
 {
     Console.WriteLine("----------< Begin All Employees >----------");
     Console.WriteLine();
@@ -233,8 +256,9 @@ static void listAllEmployees(AllEmployeesInfo info)
         Console.WriteLine("First Name: " + employee.FirstName);
         Console.WriteLine("Last Name: " + employee.LastName);
         Console.WriteLine("Username: " + employee.Credentials.Username);
+        Console.WriteLine("Is Admin?: " + employee.IsAdmin);
         Console.WriteLine();
     }
     Console.WriteLine("----------< End All Employees >----------");
     Console.WriteLine();
-}*/
+}
