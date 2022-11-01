db49a2f6e97249c4537877e6f73be45dc75529db
Dan-Yee <dyee3435@gmail.com>
2022-11-01 17:24:05 -0400

    implemented RPCs for inserting a completed procedure into the database (minus forms and images) and getting all procedures associated with a specific client by ID

--- Server/Services/ProcedureService.cs
+++ Server/Services/ProcedureService.cs
@@ -30,7 +30,7 @@ namespace Server.Services
         /// <returns>An object containing the status of the RPC and a status message.</returns>
         public override Task<ServiceStatus> addProcedure(ProcedureInfo request, ServerCallContext context)
         {
-            return Task.FromResult(insertProcedure(request));
+            return Task.FromResult(InsertProcedure(request));
         }
 
         /// <summary>
@@ -38,9 +38,40 @@ namespace Server.Services
         /// </summary>
         /// <param name="info">An object containing the client id and employee id associated with the procedure, and forms/images associated with the procedure</param>
         /// <returns>An object containing the status of the RPC and a status message.</returns>
-        private static ServiceStatus insertProcedure(ProcedureInfo info)
+        private static ServiceStatus InsertProcedure(ProcedureInfo info)
         {
             ServiceStatus status = new();
+            NpgsqlCommand command;
+            string query;
+            int procedureID;
+
+            using (NpgsqlConnection conn = GetConnection())
+            {
+                query = "INSERT INTO Client_Procedures (procedure_name, procedure_datetime, client_id, employee_id, procedure_notes) VALUES ($1, $2, $3, $4, $5) RETURNING procedure_id;";
+                command = new NpgsqlCommand(@query, conn)
+                {
+                    Parameters =
+                    {
+                        new() {Value = info.ProcedureName},
+                        new() {Value = DateTime.UtcNow},
+                        new() {Value = info.ClientID},
+                        new() {Value = info.EmployeeID},
+                        new() {Value = info.ProcedureNotes}
+                    }
+                };
+                conn.Open();
+                try
+                {
+                    procedureID = command.ExecuteNonQuery();                    // This specific query returns the procedure_id of the newly inserted procedure.
+                    status.IsSuccessfulOperation = true;
+                    status.StatusMessage = "Success: Procedure was saved.";
+                } catch (NpgsqlException pgE)
+                {
+                    status.IsSuccessfulOperation = false;
+                    status.StatusMessage = "Error: Unable to save procedure.";
+                }
+                conn.Close();
+            }
             return status;
         }
 
@@ -63,6 +94,41 @@ namespace Server.Services
         private static AllProcedures SelectAllProcedures(ClientID id)
         {
             AllProcedures procedures = new();
+            NpgsqlCommand command;
+            NpgsqlDataReader reader;
+            string query;
+
+            using (NpgsqlConnection conn = GetConnection())
+            {
+                query = "SELECT * FROM Client_Procedures WHERE client_id = $1;";
+                command = new NpgsqlCommand(@query, conn)
+                {
+                    Parameters =
+                    {
+                        new() {Value = id.CID}
+                    }
+                };
+                conn.Open();
+                reader = command.ExecuteReader();
+                if(reader.HasRows)
+                {
+                    while(reader.Read())
+                    {
+                        var currentProcedure = new ProcedureInfo
+                        {
+                            ProcedureID = Convert.ToInt32(reader["procedure_id"]),
+                            ProcedureName = reader["procedure_name"].ToString(),
+                            ProcedureDatetime = reader["procedure_datetime"].ToString(),
+                            ClientID = Convert.ToInt32(reader["client_id"]),
+                            EmployeeID = Convert.ToInt32(reader["employee_id"]),
+                            ProcedureNotes = reader["procedure_notes"].ToString()
+                        };
+                        procedures.Procedures.Add(currentProcedure);
+                    }
+                }
+                reader.Close();
+                conn.Close();
+            }
             return procedures;
         }
     }
