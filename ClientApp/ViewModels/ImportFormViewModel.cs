36fb1013c5b7199693d482adb176adc984c3c348
Hafez <hafezsedah@gmail.com>
2022-11-06 16:33:15 -0500

    Implemented file upload on client side

--- ClientApp/ViewModels/ImportFormViewModel.cs
+++ ClientApp/ViewModels/ImportFormViewModel.cs
@@ -8,11 +8,163 @@ using System.Text;
 using System.Threading.Tasks;
 using Server;
 using System.Collections.ObjectModel;
+using Google.Protobuf;
+using Grpc.Net.Client;
+using System.IO;
 
 namespace ClientApp.ViewModels
 {
+
     public class ImportFormViewModel : ReactiveObject
     {
+        
+        //global variables used to store the information corresponding to files
+        private static List<byte[]> FilesInBytes = new List<byte[]>();
+        private static string[] FileNames = new string[1];
+        private static string[] FileExtentions = new string[1];
+
+        /// <summary>
+        /// Metheod that sends a file to the server in cunks(byte[])
+        /// </summary>
+        /// <param name="bytes"> The file to send in bytes </param>
+        /// <param name="fName"> The name of the file we will be sending </param>
+        /// <param name="fExtension"> The files Extension</param>
+        /// <param name="PID"> The procedure ID corresponding to the file, if it's a template pass set to: -1</param>
+        /// <param name="destination"> Indicates to the server the table where the file should be stored set to 0 if the file iss a template </param>
+        /// <returns> Doesn't return </returns>
+        /// <exception cref="Exception"></exception>
+        private static async Task UploadFile(byte[] bytes, string fName, string fExtension, int PID, int destination)
+        {
+
+            var channel = GrpcChannel.ForAddress("https://localhost:7123");
+            ///var client = new FileTransfer.FileTransferClient(channel);
+            var client = new FileTransfer.FileTransferClient(channel);
+            var stream = client.UploadFile();
+
+            if ((destination < 0) || (destination > 2))
+            {
+                throw new Exception("inavlid Storage Destination (TableID)");
+            }
+
+            //construct the MetaData message
+            MetaData md = new MetaData();
+            md.Filename = fName;
+            md.Extension = fExtension;
+            md.ProcedureID = PID;
+            md.StorageLocation = destination;
+
+
+            //send the MetaData
+            await stream.RequestStream.WriteAsync(new UploadFileRequest { Metadata = md });
+
+            //create a MemoryStream to facilitate sending the bytes
+            MemoryStream ms = new MemoryStream(bytes);
+
+
+            try
+            {
+                int length = (int)ms.Length; // get file length
+                int buffSize = 4000000;      // set buffer size to 4MB (ie. Max)
+                int sum = 0;                 // keep track of sent bytes
+
+
+                int BytesToSend = (int)ms.Length;  //get the number of bytes to send
+
+                //if the number of bytes to send is less than 4MB
+                if (BytesToSend < buffSize)
+                {
+                    buffSize = BytesToSend;// set the buffer's size to the number of bytes to send
+                }
+
+
+                while (BytesToSend > 0) // Loop untill all the bytes are sent
+                {
+                    byte[] buffer = new byte[buffSize];   //create buffer for sending the bytes
+                    int n = ms.Read(buffer, 0, buffSize); //read into the buffer 
+
+                    //send the chunks/buffer to the server
+                    await stream.RequestStream.WriteAsync(new UploadFileRequest { ChunkData = ByteString.CopyFrom(buffer) });
+
+                    if (n == 0)
+                    {
+                        break; //all bytes are sent
+                    }
+
+                    BytesToSend -= n; //update the number of bytes to send
+                    sum += n;         //update the sum
+
+                    if (BytesToSend < buffSize)
+                    {
+                        buffSize = BytesToSend; //decrese the size of the buffer if needed when the end is reached
+                    }
+
+                }
+
+                await stream.RequestStream.CompleteAsync();           //let the server know we are done
+                ServiceStatus response = await stream.ResponseAsync; //get the response from the server
+
+                Console.WriteLine("Bytes to send: " + BytesToSend + " / Bytes sent: " + sum);
+
+                Console.WriteLine("Server responce: " + response.StatusMessage);
+            }
+            finally
+            {
+                ms.Close();
+            }
+        }
+
+
+        /// <summary>
+        /// Sets the arguments for the FileUpload method
+        /// </summary>
+        /// <param name="FilePaths"> array with the paths of all files to send</param>
+        /// <exception cref="Exception"></exception>
+        private static void SetArgs(string[] FilePaths)
+        {
+
+            for (int i = 0; i < FilePaths.Length; i++)
+            {
+                string FileNameAndExtention = null;
+                FileNameAndExtention = Path.GetFileName(FilePaths[i]);
+
+                string[] FileNE = FileNameAndExtention.Split('.');
+                if (FileNE.Length != 2)
+                {
+                    throw new Exception("INVALID FILE NAME!!");
+                }
+
+                FileNames[i] = FileNE[0];
+                FileExtentions[i] = FileNE[1];
+                byte[] FileBytes = File.ReadAllBytes(FilePaths[i]);
+                FilesInBytes.Add(FileBytes);
+            }
+
+        }
+
+        /// <summary>
+        /// The value returned serves as a flag for the server to know where to store the file in the database
+        /// </summary>
+        /// <returns></returns>
+        private static int FormTemplate()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// The value returned lets the server know that the form isn't related to any procedure
+        /// </summary>
+        /// <returns></returns>
+        private static int NotProcedureForm()
+        {
+            return -1;
+        }
+
+
+        
+
+
+
+
         //View that this viewmodel is attached to
         ImportFormView _importFormView;
         //Holds the paths of the selected files
@@ -51,6 +203,7 @@ namespace ClientApp.ViewModels
             {
                 //FilePaths = "change";
                 FilePaths = y[0];
+                SetArgs(y);
                 /*
                 foreach (var path in y)
                 {
@@ -66,10 +219,16 @@ namespace ClientApp.ViewModels
         /// <summary>
         /// Onclick event for storing the file
         /// </summary>
-        public void StoreFileCommand()
-        {
-            //Hafez here
+        /// 
 
+        public async void StoreFileCommand()
+        {
+           
+            for (int i = 0; i < FileNames.Length; i++)
+            {
+                await UploadFile(FilesInBytes[i], FileNames[i], FileExtentions[i], NotProcedureForm(), FormTemplate());
+            }
+            
 
             //This brings you back to last page
             new AdminHomeView().Show();
--- Server/Server.cs
+++ Server/Server.cs
@@ -9,6 +9,7 @@ var app = builder.Build();
 app.MapGrpcService<EmployeeService>();
 app.MapGrpcService<ClientService>();
 app.MapGrpcService<ProcedureService>();
+app.MapGrpcService<FileUploadService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
