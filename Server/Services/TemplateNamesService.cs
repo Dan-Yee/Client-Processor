e519caa45f8cf395f531f2cabda3aa4fd0d4281f
moemen34 <moemenelmegahed21@gmail.com>
2022-11-20 22:46:03 -0500

    update getting template names to not be async

--- Server/Services/TemplateNamesService.cs
+++ Server/Services/TemplateNamesService.cs
@@ -18,17 +18,54 @@ namespace GrpcServer.Services
         }
 
 
-        public override async Task GetTemplateNames(TemplatesRequest request, IServerStreamWriter<TemplateName> responseStream, ServerCallContext context)
-        {
-            List<TemplateName> templates = GetTemplateNames();    
 
-            foreach (TemplateName template in templates)
-            {
-                await responseStream.WriteAsync(template);
-            }    
-            //return null;
-            //return base.GetTemplateNames(request, responseStream, context);
+        public override Task<TemplatesResponse> GetTemplateNames(TemplatesRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(GetTemplateNames());
         }
+
+
+        /// <summary>
+        /// Method that returns photos from the database based on the received request
+        /// </summary>
+        /// <param name="PID"></param> the procedure ID corresponding to the photos
+        /// <param name="isBefore"></param> indicates whether the requested photos are taken
+        /// before of after the procedure was don
+        /// <returns>List</returns> a list of photos
+        private static TemplatesResponse GetTemplateNames()
+        {
+            NpgsqlCommand command;
+            NpgsqlDataReader reader;
+            string query;
+
+            //List to store files got from the database
+            //List<TemplateName> Templates = new List<TemplateName>();
+            TemplatesResponse Templates = new TemplatesResponse();
+
+            using (NpgsqlConnection conn = GetConnection())
+            {
+                query = $"SELECT filename, file_extension FROM empty_forms";
+                command = new NpgsqlCommand(@query, conn);
+                conn.Open();
+                reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        string TemplateName = (string)reader["filename"];
+                        string TemplateExtension = (string)reader["file_extension"];
+
+                        Templates.TemplateNames.Add(new TemplateName { FormTemplateName = TemplateName, FormTemplateExtension = TemplateExtension });
+                    }
+                }
+                reader.Close();
+                conn.Close();
+            }
+
+            return Templates;
+        }
+
 
 
         private static NpgsqlConnection GetConnection()
@@ -43,52 +80,6 @@ namespace GrpcServer.Services
 
             return new NpgsqlConnection(@connectionString);
         }
-
-        /// <summary>
-        /// Method that returns photos from the database based on the received request
-        /// </summary>
-        /// <param name="PID"></param> the procedure ID corresponding to the photos
-        /// <param name="isBefore"></param> indicates whether the requested photos are taken
-        /// before of after the procedure was don
-        /// <returns>List</returns> a list of photos
-        private static List<TemplateName> GetTemplateNames()
-        {
-            NpgsqlCommand command;
-            NpgsqlDataReader reader;
-            string query;
-
-            //List to store files got from the database
-            List<TemplateName> Templates = new List<TemplateName>();
-
-
-            using (NpgsqlConnection conn = GetConnection())
-            {
-                query = $"SELECT filename, file_extension FROM empty_forms";
-                command = new NpgsqlCommand(@query, conn);
-                conn.Open();
-                reader = command.ExecuteReader();
-
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        //read the data and create a photo object
-                        string TemplateName = (string)reader["filename"];
-                        string TemplateExtension = (string)reader["file_extension"];
-
-                        //add the photo to the List of photos
-                        Templates.Add(new TemplateName { FormTemplateName = TemplateName, FormTemplateExtension = TemplateExtension });
-                    }
-                }
-                reader.Close();
-                conn.Close();
-            }
-
-            return Templates;
-        }
-
-
-
 
     }
 }
