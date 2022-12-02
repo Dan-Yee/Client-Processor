e1ad2cd1f26ce0242249071251351351046fcbe4
Dan Yee <dyee3435@gmail.com>
2022-12-02 12:45:21 -0500

    update database table definitions to include logging changes

--- Database/Client_Procedures.sql
+++ Database/Client_Procedures.sql
@@ -1,10 +1,14 @@
 CREATE TABLE Client_Procedures (
 	procedure_id serial PRIMARY KEY,
-	procedure_name varchar(50),								-- the name of the procedure; user inputted
-	procedure_datetime timestamp,							-- the date and time of a specific procedure
-	client_ID integer,										-- the Client this procedure is associated with
-	employee_ID integer,									-- the Employee working on this procedure
-	procedure_notes varchar(10000),							-- Notes/Comments left on the procedure
-	FOREIGN KEY(client_ID) REFERENCES Clients,
-	FOREIGN KEY(employee_ID) REFERENCES Employees
+	procedure_name varchar(50),											-- the name of the procedure; user inputted
+	procedure_notes varchar(10000),										-- Notes/Comments left on the procedure
+	client_ID integer,													-- the Client this procedure is associated with
+	employee_ID integer,												-- the Employee working on this procedure
+	create_datetime timestamp DEFAULT (NOW() AT TIME ZONE 'UTC'),		-- the date and time of a specific procedure
+	last_edited_by integer,												-- the Employee who last made changes to the procedure
+	last_edited_datetime timestamp DEFAULT (NOW() AT TIME ZONE 'UTC'),	-- the date and time of the last time the procedure was updated.
+	isDeleted bool DEFAULT false,
+	FOREIGN KEY (client_ID) REFERENCES Clients,
+	FOREIGN KEY (employee_ID) REFERENCES Employees,
+	FOREIGN KEY (last_edited_by) REFERENCES Employees
 );
--- Database/Procedure_Forms.sql
+++ Database/Procedure_Forms.sql
@@ -1,8 +1,15 @@
 CREATE TABLE Procedure_Forms (
-	form_id serial PRIMARY KEY,									-- ID used to uniquely identify each form in this table
-	procedure_id integer,										-- The procedure this form is associated with
-	filename varchar(255),										-- The name of the file.
-	file_extension varchar(255),								-- The extension of the file.
-	file_bytes bytea,											-- The content of the file stored as bytes.
-	FOREIGN KEY (procedure_ID) REFERENCES Client_Procedures
+	form_id serial PRIMARY KEY,											-- ID used to uniquely identify each form in this table
+	procedure_id integer,												-- The procedure this form is associated with
+	filename varchar(255),												-- The name of the file.
+	file_extension varchar(255),										-- The extension of the file.
+	file_bytes bytea,													-- The content of the file stored as bytes.
+	created_by integer,													-- The employee that completed the form for the Client
+	create_datetime timestamp DEFAULT (NOW() AT TIME ZONE 'UTC'),		-- The date and time when the Form was first submitted
+	last_edited_by integer,												-- The employee that updated (deleted) the form
+	last_edited_datetime timestamp DEFAULT (NOW() AT TIME ZONE 'UTC'),	-- The date and time when the form was updated (deleted)
+	isDeleted bool DEFAULT false,
+	FOREIGN KEY (procedure_ID) REFERENCES Client_Procedures,
+	FOREIGN KEY (created_by) REFERENCES Employees,
+	FOREIGN KEY (last_edited_by) REFERENCES Employees
 );
