fcd85ed3ca2faf0ed9bcd5f6f999a00b1362699f
Dan Yee <dyee3435@gmail.com>
2022-12-02 12:46:06 -0500

    update photos table definition to include logging changes

--- Database/procedure_photos.sql
+++ Database/procedure_photos.sql
@@ -1,12 +1,16 @@
-CREATE TABLE Procedure_photos (
+CREATE TABLE Procedure_Photos (
     procedure_ID int,
     photo_name varchar(255),
     photo_extension varchar(255),
-    isBefore bool, 
     photo_bytes bytea,
-    CONSTRAINT procedure_ID
-      FOREIGN KEY(procedure_ID) 
-      REFERENCES client_procedures(procedure_ID),
-    CONSTRAINT PrimaryKey 
-	PRIMARY KEY (procedure_id, photo_name)
+	isBefore bool,
+	created_by integer,													-- The employee that uploaded this photo
+	create_datetime timestamp DEFAULT (NOW() AT TIME ZONE 'UTC'),		-- The date and time this photo was uploaded
+	last_edited_by integer,												-- The employee that last edited (deleted) this photo
+	last_edit_datetime timestamp DEFAULT (NOW() AT TIME ZONE 'UTC'),	-- The date and time this photo was edited (deleted)
+	isDeleted bool DEFAULT false,
+	PRIMARY KEY (procedure_ID, photo_name),
+	FOREIGN KEY (procedure_ID) REFERENCES Client_Procedures,
+	FOREIGN KEY (created_by) REFERENCES Employees,
+	FOREIGN KEY (last_edited_by) REFERENCES Employees
 );
