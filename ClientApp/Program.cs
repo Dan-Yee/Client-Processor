9b600ba505adfb4451ed3994985479d594cc2a87
Dan Yee <dyee3435@gmail.com>
2022-12-04 16:54:12 -0500

    defined grpc channel as a static

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -1,12 +1,16 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.ReactiveUI;
+using Grpc.Net.Client;
 using System;
 
 namespace ClientApp
 {
     internal class Program
     {
+        // Global gRPC channel for Server connection
+        public static GrpcChannel gRPCChannel = GrpcChannel.ForAddress("https://localhost:7123");
+
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
